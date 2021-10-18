8d03162150bf28ac7c1f91b678dcca08c329810e
ruthenian8 <ruthenian8@gmail.com>
2021-10-18 18:03:56 +0300

    add file

--- preprocessing/command.sh
+++
@@ -1,4 +0,0 @@
-awk -F, '{ print $1 }' chosen.csv | hfst-proc ../merged.tr.hfstol > ./sampledAnas.txt
-sed 's/^[^/]*\///g' sampledAnas.txt > /tmp/temp.txt
-cat /tmp/temp.txt > ./sampledAnas.txt
-rm /tmp/temp.txt
